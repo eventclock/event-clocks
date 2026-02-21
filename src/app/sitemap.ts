6393709d7b263b93bd811f903bf08985a9a521ca
eventclock <event.clock29@gmail.com>
2026-02-21 01:33:52 -0800

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -24,6 +24,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+     {
+      url: `${BASE}/business-days`,
+      lastModified: lastMod,
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
     {
       url: `${BASE}/about`,
       lastModified: lastMod,
