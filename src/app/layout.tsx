6afb632bee1fb708e96c33547b73b1f8e7aa0381
eventclock <event.clock29@gmail.com>
2026-03-06 03:51:55 -0800

    took out adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,13 +47,13 @@ export default function RootLayout({
             gtag('config', 'G-B2HX3GQVYF');
           `}
         </Script>
-        <Script
+        {/* <Script
           id="adsense"
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8550123052003634"
           strategy="afterInteractive"
           crossOrigin="anonymous"
-        />
+        /> */}
       </head>
 
       <body
